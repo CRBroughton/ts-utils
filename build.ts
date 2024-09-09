5a77cedd1c0b32f58c7326235b84c0ac70758241
CRBroughton <crbroughton@posteo.uk>
2024-09-09 19:15:50 +0100

    chore: :construction_worker: update build script to support multiple dirs

--- build.ts
+++ build.ts
@@ -1,7 +1,28 @@
+import { dirname, join } from 'node:path'
+import { existsSync, mkdirSync } from 'node:fs'
 import dts from 'bun-plugin-dts'
 
-await Bun.build({
-  entrypoints: ['./index.ts'],
-  outdir: 'dist',
-  plugins: [dts()],
-})
+const entrypoints = [
+  './src/await/index.ts',
+  './src/enum/index.ts',
+  './src/utils/index.ts',
+]
+
+function ensureDirectoryExists(path: string) {
+  const dir = dirname(path)
+  if (!existsSync(dir))
+    mkdirSync(dir, { recursive: true })
+}
+
+await Promise.all(
+  entrypoints.map((entry) => {
+    const outputDir = join('dist', entry.replace('./src/', '').replace('/index.ts', ''))
+    ensureDirectoryExists(outputDir)
+
+    return Bun.build({
+      entrypoints: [entry],
+      outdir: outputDir,
+      plugins: [dts()],
+    })
+  }),
+)
