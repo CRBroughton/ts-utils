bcad5a0e8b3f2b6113f3666e4180ac393e2df296
CRBroughton <crbroughton@posteo.uk>
2024-09-09 20:03:57 +0100

    chore: :green_heart: fix build script

--- build.ts
+++ build.ts
@@ -1,28 +1,7 @@
-import { dirname, join } from 'node:path'
-import { existsSync, mkdirSync } from 'node:fs'
 import dts from 'bun-plugin-dts'
 
-const entrypoints = [
-  './src/await/index.ts',
-  './src/enum/index.ts',
-  './src/utils/index.ts',
-]
-
-function ensureDirectoryExists(path: string) {
-  const dir = dirname(path)
-  if (!existsSync(dir))
-    mkdirSync(dir, { recursive: true })
-}
-
-await Promise.all(
-  entrypoints.map((entry) => {
-    const outputDir = join('dist', entry.replace('./src/', '').replace('/index.ts', ''))
-    ensureDirectoryExists(outputDir)
-
-    return Bun.build({
-      entrypoints: [entry],
-      outdir: outputDir,
-      plugins: [dts()],
-    })
-  }),
-)
+await Bun.build({
+  entrypoints: ['./index.ts'],
+  outdir: 'dist',
+  plugins: [dts()],
+})
---
+++ index.ts
@@ -0,0 +1,14 @@
+export {
+  type Handlers,
+  handleError,
+  safeAwait,
+} from './src/await'
+
+export {
+  type EnumLike,
+  createEnum,
+} from './src/enum'
+
+export {
+  type Prettify,
+} from './src/utils'
